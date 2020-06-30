fea66881b5bbf95c03aa6d064b605faa208b3412
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-30 10:41:01 -0400

    Creating manageGoal reducer

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
-
 import thunk from 'redux-thunk'
 //compose allows you to add more than one middleware
 import {createStore, compose, applyMiddleware} from 'redux'
 //anything that we want to have access to the store we wrap with Provider
 import {Provider} from 'react-redux'
+import manageGoal from './reducers/manageGoal'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(manageGoal, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
