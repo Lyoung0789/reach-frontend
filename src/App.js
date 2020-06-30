aa7a102d09c18dc11c3a090456329a90a570bd71
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-29 23:26:55 -0400

    Commenting out the componentDidMount for now

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  componentDidMount(){
-    fetch('http://localhost:3000/api/v1/goals')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-    })
-  }
+  // componentDidMount(){
+  //   fetch('http://localhost:3000/api/v1/goals/1')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(data)
+  //   })
+  // }
+  
   render() {
     return (
       <div className="App">
