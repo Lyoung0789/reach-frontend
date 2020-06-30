a30f0b15304e3d568d03e6d6f4b11b8c2be46dc0
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-30 14:05:28 -0400

    Added more styling to each goal

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -3,16 +3,17 @@ import { Card } from 'react-bootstrap';
 
 
 export default class Goal extends Component {
+    //This is where i can add my specfic url for pictures.
 	render() {
 		return (
 			<Card style={{ width: '18rem' }}>
-				<Card.Img variant="top" src="https://picsum.photos/100/100" />
-				<Card.Body>
+				<Card.Img variant="top" src="https://images.freeimages.com/images/large-previews/4f7/blank-sign-bullet-holes-1447150.jpg" />
+				<Card.ImgOverlay>
                 <Card.Title>{this.props.goal.title}</Card.Title>
 					<Card.Text>
 						{this.props.goal.description}
     				</Card.Text>
-				</Card.Body>
+				</Card.ImgOverlay>
 			</Card>
 		)
 	}
