f53c5f61260ef86ba71f49618fc918e901d74bbc
Lyoung0789 <Lestermyoung07@gmail.com>
2020-07-01 01:16:32 -0400

    Successfully linked show page for each Goal

--- src/components/GoalShow.js
+++ src/components/GoalShow.js
@@ -3,13 +3,19 @@ import React from 'react'
 
 const GoalShow = (props) => {
     // console.log(props)
-    let goal = props.goals[props.match.params.id -1]
     // debugger
+    let goal = props.goals.find(element => element.id == props.match.params.id)
+    debugger
     
 
     if (goal){
         return (
-            <div>{goal ? goal.title : null}</div>
+        <div>
+            {goal.title} - {goal.description}
+            <p>
+                {goal.todos.map(todo => <li>{todo.title}</li>)}
+            </p>
+        </div>
         )
     } else {
         return (
