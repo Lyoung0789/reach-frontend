aff59741125626ac9728248c983b9a21ce48dd96
Lyoung0789 <Lestermyoung07@gmail.com>
2020-07-01 22:40:37 -0400

    fixing routing in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,11 @@ import {NavLink} from 'react-router-dom'
 const NavBar = () => {
     return (
         <Navbar bg="dark" variant="dark">
-            <Navbar.Brand as={NavLink} to="/goals">Reach App</Navbar.Brand>
+            <Navbar.Brand as={NavLink} to="/">Reach App</Navbar.Brand>
             <Nav className="mr-auto">
+                <Nav.Link as={NavLink} to="/goals">Home</Nav.Link>
                 <Nav.Link as={NavLink}to="/goals/new" >New Goal</Nav.Link>
-
-                <Nav.Link href="/goals/">Pricing</Nav.Link>
+                <Nav.Link href="/goals/">Reached Goals</Nav.Link>
             </Nav>
             <Form inline>
                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
