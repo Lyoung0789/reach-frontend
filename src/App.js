85b857aff0b1e8bc8b17c2abc455af5269cb210b
Lyoung0789 <Lestermyoung07@gmail.com>
2020-07-06 10:32:17 -0400

    Adding welcome page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import GoalsContainer from './containers/GoalsContainer'
 import NavBar from './components/NavBar/NavBar'
 import {Route} from 'react-router-dom'
+import Welcome from './components/Welcome'
 
 
 class App extends Component {
@@ -10,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <Route path='/' render ={(routerProps) => <NavBar {...routerProps}/>}/>
-          
+        <Route exact path='/' component={Welcome} />
         <GoalsContainer />
         
       </div>
---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Welcome = () => {
+    return(
+        <div>
+            <h1>Welcome to Reach</h1>
+            <p>
+                Reach is an app developed to track goals along with pending action items, featuring a checklist to help you achieve your targets.
+            </p>
+        </div>
+    )
+}
+
+export default Welcome
