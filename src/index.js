8c89db2ecad052522631b6fc298b97f193f4b7cd
Lyoung0789 <Lestermyoung07@gmail.com>
2020-07-13 23:49:20 -0400

    removing comments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+
 import App from './App';
 import thunk from 'redux-thunk'
-//compose allows you to add more than one middleware
+
 import {createStore, compose, applyMiddleware} from 'redux'
-//anything that we want to have access to the store we wrap with Provider
+
 import {Provider} from 'react-redux'
 import manageGoal from './reducers/manageGoal'
 import {BrowserRouter as Router} from 'react-router-dom'
